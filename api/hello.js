b40d055cb903b986d99e74b2aa785f480bff6746
tb-lab <74849323+tb-lab@users.noreply.github.com>
2021-01-01 12:08:07 +0800

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -34,9 +34,9 @@ var req = https.request(options, function (res) {
     var body = Buffer.concat(chunks);
     console.log(body.toString());
     const dom = new JSDOM(body.toString())
-    let price = Math.min(...[...dom.window.document.querySelectorAll(".priceSymbol>.price")].map(a=>parseInt(a.textContent.replace(",",""))))
-    console.log(dom.window.document.querySelectorAll(".priceSymbol>.price"))
-    httpRes.status(200).send(dom.window.document.querySelectorAll(".priceSymbol>.price"))
+    let price = Math.min(...[...dom.window.document.querySelector(".priceSymbol>.price")].map(a=>parseInt(a.textContent.replace(",",""))))
+    console.log(dom.window.document.querySelector(".priceSymbol>.price"))
+    httpRes.status(200).send(dom.window.document.querySelector(".priceSymbol>.price"))
     httpRes.status(200).send(price.toString())
 
   });
