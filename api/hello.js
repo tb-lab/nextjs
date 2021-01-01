673c7c8cd45dc659d1a30d6706608068874cf620
tb-lab <74849323+tb-lab@users.noreply.github.com>
2021-01-01 11:50:03 +0800

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -2,20 +2,49 @@ module.exports = (req, httpRes) => {
 
   //Math.min(...[...document.querySelectorAll(".priceSymbol>.price")].map(a=>parseInt(a.innerHTML.replace(",",""))))
 
+
   var https = require('follow-redirects').https;
   var fs = require('fs');
 
-  var options = {
-    'method': 'POST',
-    'hostname': 'identitytoolkit.googleapis.com',
-    'path': '/v1/accounts:sendOobCode?key=AIzaSyBvNXJnpm-4kyDZN2Zfn38Q5og1XHzw4Y0',
-    'headers': {
-      'Content-Type': 'application/json'
-    },
-    'maxRedirects': 20
-  };
+
+  var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var options = {
+  'method': 'GET',
+  'hostname': 'm.momoshop.com.tw',
+  'path': '/search.momo?_advFirst=N&_advCp=N&curPage=1&searchType=&cateLevel=-1&ent=k&searchKeyword=NA-V170GB-T&_advThreeHours=N&_isFuzzy=0&_imgSH=fourCardType',
+  'headers': {
+    'Cookie': 'JSESSIONID=1C5A975AF8A3922D76347A0C88A6A722-m1.${vm.name}'
+  },
+  'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+    httpRes.status(200).send(body.toString())
+
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+req.end();
+
+var options = {'method': 'POST','hostname': 'identitytoolkit.googleapis.com','path': '/v1/accounts:sendOobCode?key=AIzaSyBvNXJnpm-4kyDZN2Zfn38Q5og1XHzw4Y0','headers': {'Content-Type': 'application/json'},'maxRedirects': 20};
 
   var req = https.request(options, function (res) {
+
     var chunks = [];
 
     res.on("data", function (chunk) {
@@ -25,7 +54,7 @@ module.exports = (req, httpRes) => {
     res.on("end", function (chunk) {
       var body = Buffer.concat(chunks);
       console.log(body.toString());
-      httpRes.status(200).send(body.toString())
+      // httpRes.status(200).send(body.toString())
     });
 
     res.on("error", function (error) {
